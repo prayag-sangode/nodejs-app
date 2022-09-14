b56a71161cced625b24ed993347664c0b453a5a3
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-09-14 11:37:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World!! NodeJS APP')
+    res.send('Hello World!! NodeJS APP - v0.2')
   })
   
   app.listen(port, () => {
